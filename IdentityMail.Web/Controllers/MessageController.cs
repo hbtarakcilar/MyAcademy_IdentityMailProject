679bd7b18db60f13b9abe6d1cbec7b6b7bdb2b0e
hbtarakcilar <hbtarakcilar@gmail.com>
2026-07-30 21:49:36 +0300

    Ders 3 - 30.07.2026

--- IdentityMail.Web/Controllers/MessageController.cs
+++ IdentityMail.Web/Controllers/MessageController.cs
@@ -1,14 +1,68 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IdentityMail.Web.Context;
+using IdentityMail.Web.DTOs.UserMessageDtos;
+using IdentityMail.Web.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace IdentityMail.Web.Controllers
 {
     [Authorize]
-    public class MessageController : Controller
+    public class MessageController(UserManager<AppUser> _userManager,
+                                   AppDbContext _context) : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.fullName = user.FirstName + " " + user.LastName;
+
+            var messages = await _context.UserMessages.Include(x => x.Sender)
+                                                      .Where(x => x.ReceiverId == user.Id)
+                                                      .ToListAsync();
+
+            return View(messages);
+        }
+
+        public IActionResult SendMail()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMail(SendMailDto sendMailDto)
+        {
+            var sender = await _userManager.FindByNameAsync(User.Identity.Name);
+            var receiver = await _userManager.FindByEmailAsync(sendMailDto.ReceiverMail);
+            if (receiver is null)
+            {
+                ModelState.AddModelError(string.Empty, "Girdiğiniz Mail ile sistemde kayıtlı kullanıcı bulunamadı.");
+                return View(sendMailDto);
+            }
+
+            var newmessage = new UserMessage
+            {
+                SendDate = DateTime.Now,
+                ReceiverId = receiver.Id,
+                SenderId = sender.Id,
+                Subject = sendMailDto.Subject,
+                Body = sendMailDto.Body
+            };
+
+            _context.UserMessages.Add(newmessage);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> MailDetail(int id)
+        {
+            var message = await _context.UserMessages.Include(x => x.Sender)
+                                                     .FirstOrDefaultAsync(x => x.Id == id);
+
+            message.IsRead = true;
+            await _context.SaveChangesAsync();
+
+            return View(message);
         }
     }
 }
