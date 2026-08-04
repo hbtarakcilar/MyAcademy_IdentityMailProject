bd20f40f79431bcb9c722dfab7534dcdfc8da3e0
hbtarakcilar <hbtarakcilar@gmail.com>
2026-08-04 08:59:21 +0300

    MainLayout - Inbox - MailDetail

---
+++ IdentityMail.Web/ViewComponents/Default-Index/_DefaultHeaderViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using IdentityMail.Web.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+public class _DefaultHeaderViewComponent(UserManager<AppUser> _userManager) : ViewComponent
+{
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+
+        ViewBag.ProfileImage = user.ProfileImageUrl;
+
+        return View();
+    }
+}
---
+++ IdentityMail.Web/ViewComponents/Default-Index/_DefaultSidebarViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using IdentityMail.Web.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+public class _DefaultSidebarViewComponent(UserManager<AppUser> _userManager) : ViewComponent
+{
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+
+        ViewBag.FullName = $"{user.FirstName} {user.LastName}";
+        ViewBag.ProfileImage = user.ProfileImageUrl;
+
+        return View();
+    }
+}
